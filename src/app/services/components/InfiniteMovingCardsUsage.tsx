6d66a609f28710186b6b131977737bcbfab83c9e
Rishi <rishikm215@gmail.com>
2026-02-17 18:16:53 +0530

    final changes

--- src/app/services/components/InfiniteMovingCardsUsage.tsx
+++ src/app/services/components/InfiniteMovingCardsUsage.tsx
@@ -1,10 +1,13 @@
 import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
+import { ReviewTestimonial } from "../interfaces";
 
+interface InfiniteMovingCardsUsageProps {
+  testimonials: ReviewTestimonial[];
+}
 
-
-export function InfiniteMovingCardsUsage({testimonials}) {
+export function InfiniteMovingCardsUsage({ testimonials }: InfiniteMovingCardsUsageProps) {
   return (
-    <div className="h-160 rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+    <div className="h-40 rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
         items={testimonials}
         direction="right"
